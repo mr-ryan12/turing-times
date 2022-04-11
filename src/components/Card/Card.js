6b44da8498d1d822c553fcc9271476e4fb99b674
Ryan McBride <mr.ryan444@gmail.com>
2022-04-11 16:00:11 -0600

    Align elements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,15 +7,15 @@ const Card = props => {
 
   return (
     <section className="article-card" key={props.id}>
-        <section className="image-title-container">
-          <img src={props.multimedia[2].url} alt="Article Thumbnail" className="article-thumbnail"/>
-          <h2 className="article-title">{props.title}</h2>
-        </section>
-        <section className="card-details-container">
-          <p>{formattedDate}</p>
-          <p>{props.byline}</p>
-        </section>
+      <section className="image-title-container">
+        <img src={props.multimedia[2].url} alt="Article Thumbnail" className="article-thumbnail"/>
+        <h2 className="article-title">{props.title}</h2>
       </section>
+      <section className="card-details-container">
+        <p>{formattedDate}</p>
+        <p>{props.byline}</p>
+      </section>
+    </section>
   )
 }
 
