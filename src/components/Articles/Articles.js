f1538c2854f3108736ba1fd23bdb60506c880be9
Ryan McBride <mr.ryan444@gmail.com>
2022-04-11 15:24:45 -0600

    Add thumbnail picture

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -5,6 +5,7 @@ const Articles = props => {
   const allArticles = props.articles.map(article => {
     return (
       <section className="article-card" key={article.id}>
+        <img src={article.multimedia[2].url} alt="Article Thumbnail" className="article-thumbnail"/>
         <h2>{article.title}</h2>
         <p>{article.section}</p>
         <p>{article.published_date}</p>
