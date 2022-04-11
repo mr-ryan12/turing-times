255698d0fe3fddb3ee3de973dc0b5c3c2dc5490a
Ryan McBride <mr.ryan444@gmail.com>
2022-04-11 14:07:50 -0600

    Set up initial cypress test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header/Header'
-import './App.css';
+// import Header from './components/Header/Header'
+import './App.scss';
 
 const App = () => {
   return (
