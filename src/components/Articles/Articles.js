f1cc28dd670bab44deb6ff68649a8cb00258805e
Ryan McBride <mr.ryan444@gmail.com>
2022-04-12 10:06:26 -0600

    Create helper function to render article cards

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -4,42 +4,32 @@ import './Articles.scss';
 
 const Articles = props => {
   const cleanedArticles = props.articles.filter(article => article.multimedia !== null);
-  const cleanedFilteredArticles = props.filteredArticles.filter(article => article.multimedia !== null)
+  const cleanedFilteredArticles = props.filteredArticles.filter(article => article.multimedia !== null);
+
+  const createArticleCards = articles => {
+    if (articles) {
+      return articles.map((article, index) => {
+        return (
+          <Card
+            key={index}
+            id={index}
+            multimedia={article.multimedia}
+            title={article.title}
+            published_date={article.published_date}
+            byline={article.byline}
+            findArticle={props.findArticle}
+            short_url={article.short_url}
+          />
+        )
+      })
+    }
+  }
+
   const checkArticles = () => {
     if (props.isFiltered) {
-      return (
-        cleanedFilteredArticles.map((article, index) => {
-          return (
-            <Card
-              key={index}
-              id={index}
-              multimedia={article.multimedia}
-              title={article.title}
-              published_date={article.published_date}
-              byline={article.byline}
-              findArticle={props.findArticle}
-              short_url={article.short_url}
-            />
-          )
-        })
-      )
+       return createArticleCards(cleanedFilteredArticles)
     } else if (!props.isFiltered || props.selection === 'all articles') {
-      return (
-        cleanedArticles.map((article, index) => {
-          return (
-            <Card
-              key={index}
-              id={index}
-              multimedia={article.multimedia}
-              title={article.title}
-              published_date={article.published_date}
-              byline={article.byline}
-              findArticle={props.findArticle}
-              short_url={article.short_url}
-            />
-          )
-        })
-      )
+      return createArticleCards(cleanedArticles)
     }
   }
 
